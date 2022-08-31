19f9b34323b3c9fa93c69ba5e5cbb4eb7556e1e2
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-08-31 08:35:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# print("Hello world")
+# print("Då")
+# print("2022-12-24")
+# print(2022-12-24)
+
+
+e = "Holmberg"
+a = "Stefan"
+print(e+ ", " + a) # Holmberg, Stefan
+total = e+a
+
+
+print("Du heter " + e + "," + a)
+print("Du heter " ,e,a)
+
+Age = 50
+print("Vad heter du?")
+namnet = input("")
+print("Ok...")
+print("Hejsan ", namnet)
+yearsToAdd = input("Hur många år till du lägga till")
+Age = Age + int(yearsToAdd)
+print(Age)
